a14d31ac0ec3d2561e3e9b559a29cd2c95c7608f
Devamitta <devamitta@sasanarakkha.org>
2025-03-26 20:10:20 +0800

    latest changes 2025-03-26

--- patimokkha_dict.py
+++ patimokkha_dict.py
@@ -15,9 +15,6 @@ DB_DIR=MAIN_DIR + "/json"
 ## Read ods files and generate DF
 df = pd.read_excel("original_sources/Pātimokkha Word by Word.ods", engine="odf")
 
-# Replace "\n" with "<br>" in the original document
-df = df.replace("\n", "<br>")
-
 #Create main dir 
 os.makedirs(MAIN_DIR, exist_ok=True)
 
@@ -37,11 +34,10 @@ sources_json = [sj for sj in sources_json if sj["source"] is not None]
 for sj in sources_json:
     os.makedirs(MAIN_DIR + "/" + sj["source"], exist_ok=True)
     result_df = df[df["source"] == sj["source"]]
-    filtered_result_df = result_df[["abbrev", "source", "sentence", "pali_1", "pos", 
-    "grammar", "case", "meaning", "meaning_lit", "root", "base", "construction", 
-    "compound_type", "compound_construction"]]
-    filtered_result_df = filtered_result_df.fillna("")
-    filtered_result_df = filtered_result_df.astype(str)
+    result_df = result_df[["abbrev", "source", "sentence", "pali_1", "pos", 
+        "grammar", "case", "meaning", "meaning_lit", "root", "base", "construction", 
+        "compound_type", "compound_construction"]]
+    filtered_result_df = result_df.fillna("")
     filtered_result_df.to_json(DB_DIR + "/" + sj["source"] + 
         ".json", force_ascii=False, orient='records', indent=2)
 
@@ -114,12 +110,11 @@ for source_file in sources_json:
         if ln["sentence"] != "":
             file.write("<b style=\"font-size:20px\" id=\"" + 
             ln["sentence"].replace(" ", "_") + "\">" + ln["sentence"] + "</b><br>\n")
-            define_df = line_df[line_df["sentence"] == ln["sentence"]][["pali_1", 
+            define_df = (line_df[line_df["sentence"] == ln["sentence"]])[["pali_1", 
                 "pos", "grammar", "case", "meaning", "meaning_lit", "root", "base", 
                 "construction", "compound_type", "compound_construction"]]
-            define_df = define_df.fillna("")
-            define_df = define_df.astype(str)
-            definition_table = define_df.to_html(justify='left', 
+            define_df_filtered = define_df.fillna("")
+            definition_table = define_df_filtered.to_html(justify='left', 
                 index=False).replace("0", "")
             definition_table = definition_table.replace("pali_1", "pāḷi")
             
